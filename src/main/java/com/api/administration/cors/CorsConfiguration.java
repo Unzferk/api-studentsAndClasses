26fddaa1c3270dcc06d7245ba33612f9562a680a
Fernando Unzueta <fernando.unzueta.dev@gmail.com>
2022-08-31 10:35:43 -0400

    [Task-5] Add cors configurations

---
+++ src/main/java/com/api/administration/cors/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.api.administration.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer
+{
+
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins(allowedOrigin)
+                .allowCredentials(false);
+    }
+}
